8b3b17bf0291a86ff388003d0ef2949a51baabec
fullStack-Blake <koreanincanada92@gmail.com>
2020-09-25 16:36:13 +0900

    minor fix

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post("/api/sales", (req, res) => {
   myData
     .addNewSale(req.body)
     .then(data => res.json(data))
-    .catch(err => console.log(`Error when add New sales ${err}`));
+    .catch(err => res.send(`Error when add New sales ${err}`));
 });
 
 // GET /api/sales (NOTE: This route must accept the numeric query parameters "page" and "perPage", ie: /api/sales?page=1&perPage=5 )
@@ -42,7 +42,7 @@ app.get("/api/sales", (req, res) => {
   myData
     .getAllSales(req.query.page, req.query.perPage)
     .then(data => res.json(data))
-    .catch(err => console.log(`Error when loading all sales ${err}`));
+    .catch(err => res.send(`Error when loading all sales ${err}`));
 });
 
 // GET /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8)
@@ -51,7 +51,7 @@ app.get("/api/sales/:id", (req, res) => {
   myData
     .getSaleById(req.params.id)
     .then(data => res.json(data))
-    .catch(err => `Error when getting sale by id ${err}`);
+    .catch(err => res.send(`Error when getting sale by id ${err}`));
 });
 
 // PUT /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8 as well as read the contents of the request body)
@@ -60,7 +60,7 @@ app.put("/api/sales/:id", (req, res) => {
   myData
     .updateSaleById(req.body, req.params.id)
     .then(data => res.json(data))
-    .catch(err => `Error when Update existing sale ${err}`);
+    .catch(err => res.send(`Error when Update existing sale ${err}`));
 });
 
 // DELETE /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8)
@@ -69,7 +69,7 @@ app.delete("/api/sales/:id", (req, res) => {
   myData
     .deleteSaleById(req.params.id)
     .then(data => res.json(data))
-    .catch(err => `Error when deleting sale`);
+    .catch(err => res.send(`Error when deleting sale: ${err}`));
 });
 
 // ************* Initialize the Service & Start the Server
